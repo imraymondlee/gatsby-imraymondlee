842bad6d2ddb82b219ee41caec806506caa95ddc
imraymondlee <imleeraymond@gmail.com>
2021-05-13 23:25:56 -0400

    Create landing section

---
+++ src/components/Landing.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../images/landing-img.svg';
+
+const StyledLanding = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin: 5rem auto;
+`;
+
+const StyledTitle = styled.h1`
+  margin-bottom: 1rem;
+  font-size: 3.052rem;
+  color: #d62e00;
+`;
+
+const StyledSubtitle = styled.p`
+  margin-top: 0;
+  font-size: 1.25rem;
+  font-weight: 400;
+`;
+
+const StyledLandingImage = styled.img`
+  width: 35%;
+`;
+
+const IndexPage = () => {
+  return (
+    <StyledLanding>
+      <div>
+        <StyledTitle>Title Here</StyledTitle>
+        <StyledSubtitle>Subtitle Here</StyledSubtitle>
+      </div>
+      <StyledLandingImage src={logo} alt="" />
+    </StyledLanding>
+  );
+};
+
+export default IndexPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+import Landing from '../components/Landing';
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>hello world</h1>
+      <Landing />
     </Layout>
   );
 };
